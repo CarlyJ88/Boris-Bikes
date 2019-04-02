da479614e77372c9399fd3aefbc731fe7e63c3b8
Carly <carly.jenkinson65@googlemail.com>
2019-04-02 16:21:03 +0100

    first rspec test passed

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,5 @@
+require 'docking_station'
+describe DockingStation do
+    it {is_expected.to respond_to :release_bike}
+end
+   # Add a test to your spec file that expects DockingStation instances to respond_to the method release_bike
